54500d2ee2ecda2767405c904f8c4715a025070f
YardenNahum <yardennahum2@gmail.com>
2026-04-10 12:59:42 +0300

    Update vite.config.js

--- XAI_Project/Frontend/vite.config.js
+++ XAI_Project/Frontend/vite.config.js
@@ -4,5 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist', // output directory for the build
+    emptyOutDir: true, // clear the output directory before building
+  },
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
+  proxy: {
+    '/api': { // proxy api requests to the server
+    target: 'http://localhost:3000/', //  target server
+    changeOrigin: true,
+    },
+  },
+
 })
