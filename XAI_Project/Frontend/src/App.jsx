f6b38a24e71546add4c235fe923a4e6578cfae4e
YardenNahum <yardennahum2@gmail.com>
2026-04-10 16:56:49 +0300

    changed routing

--- XAI_Project/Frontend/src/App.jsx
+++ XAI_Project/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Navbar from '../Components/NavigationBar.jsx' 
 import AboutPage from '../Components/Pages/AboutPage.jsx';
 import DomainPage from '../Components/Pages/SystemsPage.jsx';
@@ -8,19 +8,12 @@ import { StudyProvider } from '../Components/StudyContext';
 export default function App() {
   return (
     <StudyProvider>
-      <BrowserRouter>
         <div className="min-h-screen bg-slate-100 flex flex-col font-sans">
           <Navbar /> 
           <main className="flex-1 p-4">
-            <Routes>
-              <Route path="/" element={<Navigate to="/about" replace />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/thank-you" element={<FinalPage />} />
-              <Route path="/:domainId" element={<DomainPage />} />
-            </Routes>
+             <Outlet />
           </main>
         </div>
-      </BrowserRouter>
     </StudyProvider>
   );
 }
--- XAI_Project/Frontend/src/main.jsx
+++ XAI_Project/Frontend/src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom'
+import AboutPage from '../Components/Pages/AboutPage.jsx';
+import FinalPage from '../Components/Pages/FinalPage.jsx';
+import DomainPage from '../Components/Pages/SystemsPage.jsx';
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        // Redirects the root path (/) to the about page
+        index: true,
+        element: <Navigate to="/about" replace />
+      },
+      {
+        path: "about",
+        element: <AboutPage />
+      },
+      {
+        path: "thank-you",
+        element: <FinalPage />
+      },
+      {
+        path: ":domainId",
+        element: <DomainPage />
+      }
+    ]
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
